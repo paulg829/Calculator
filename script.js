bbba27ac87346a7f75a0f28ae971da1e44b587a1
paul829 <pgarofalo880@gmail,com>
2026-07-31 18:06:17 -0500

    calculator operator functions

---
+++ script.js
@@ -0,0 +1,64 @@
+function add(a,b){
+
+    return a+b;
+
+}
+
+
+function subtract(a,b){
+
+    return a-b;
+
+}
+
+
+function multiply(a,b){
+
+    return a*b;
+
+}
+
+
+function divide(a,b){
+
+    if(b === 0){
+
+        return "Invalid";
+
+    }
+
+    return a/b;
+
+}
+
+function operate(operator,a,b){
+
+
+    if(operator === "+"){
+
+        return add(a,b);
+
+    }
+
+
+    else if(operator === "-"){
+
+        return subtract(a,b);
+
+    }
+
+
+    else if(operator === "*"){
+
+        return multiply(a,b);
+
+    }
+
+
+    else if(operator === "/"){
+
+        return divide(a,b);
+
+    }
+
+}
